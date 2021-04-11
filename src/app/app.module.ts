a95be5cbd8e7a5d078af3274f341b8ec0a1b0d71
Sümeyye KILIÇ <smyyekilic@gmail.com>
2021-04-11 22:33:34 +0300

    angular ile frontend ve component altyapısı oluş.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './component/product/product.component';
+import { CategoryComponent } from './component/category/category.component';
+import { NaviComponent } from './component/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/product/product.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  product1:any={productId:1, productName:'defter',categoryId:1, unitPrice:5}
+  product2:any={productId:2, productName:'kalem',categoryId:1, unitPrice:5}
+  product3:any={productId:3, productName:'silgi',categoryId:1, unitPrice:5}
+  product4:any={productId:4, productName:'fırça',categoryId:1, unitPrice:5}
+  product5:any={productId:5, productName:'boya',categoryId:1, unitPrice:5}
+
+  products =[
+    this.product1,
+    this.product2,
+    this.product3,
+    this.product4,
+    this.product5,
+  ] //elimde 5 elemandan oluşan ürünler
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
